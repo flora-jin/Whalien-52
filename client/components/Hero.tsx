94d1962ce7e0dc4def32d7946126142c4f8ff703
Sophia <66497192+sophiadt@users.noreply.github.com>
2025-10-04 13:27:46 -0700

    base

---
+++ client/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div className="pb-20 pt-36">
+            <div className="text-white">
+                Whalien 52
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
